c4cd7779bb7efc7b10ecec342626e3e7824a1dfa
lkjyoonok <24701526-lkjyoonok@users.noreply.replit.com>
2023-07-16 07:54:09 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet():
+  print("Hello Angela")
+  print("How do you do Jack Bauer?")
+  print("Isn't the weather nice today?")
+
+greet()
